229554f3ff337f64c4d8d998e893914e61003c3c
niuzhihuan <zhihuan.niu@funi.com>
2017-11-22 10:05:41 +0800

    Integer

---
+++ src/v1/ch05/IntergerTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package v1.ch05;
+
+/**
+ * @author zhihuan.niu
+ * @date 2017-11-22 10:02:11 AM
+ * @Description: TODO
+ */
+public class IntergerTest {
+
+	/**
+	 * @Description: TODO
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a=100;
+		Integer b=100;
+		System.out.println(a==b);
+		a=1000;
+		b=1000;
+		System.out.println(a==b);
+		
+	}
+
+}
---
+++ src/v1/ch05/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author zhihuan.niu
+ * @date 2017-11-22 9:21:13 AM
+ * @Description: TODO
+ */
+package v1.ch05;
