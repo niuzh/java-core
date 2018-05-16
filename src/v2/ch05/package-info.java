3ec47ea68b6cfcf18606616009d3921fbf25a880
niuzhihuan <zhihuan.niu@funi.com>
2018-05-16 12:07:49 +0800

    Swing

---
+++ src/v2/ch05/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * 国际化
+ * 	介绍了如何编写国际化的程序，如何将日期、时间，数字、文本和图形用户界面本地化。
+ * Locales：描述了一个语言，位置方言等规则。只要提供了语言代码或语言和国家代码就可以构建Locale对象了。
+ * 数字格式：由Locale对象创建格式器对象，使用格式器对象进行格式化和解析工作。
+ * 日期和时间：DateFormat
+ * 排序
+ * 	排序强度：字符间的差别强度。
+ * 	分解
+ * 消息格式化：MessageFormat类格式化带变量的文本。
+ * 	文本格式转换:	native2ascii -encoding gbk 雪鹰领主windows.txt 雪鹰领主ascii.txt 
+ * 			    native2ascii -reverse 雪鹰领主ascii.txt 雪鹰领主linux.txt
+ * 文本文件和字符集
+ * 资源包
+ * 	定位资源包
+ * 	属性文件：存储属性的文件都是ASCII格式文件。
+ * 	包类：提供字符串以外的资源。
+ * 一个完整的例子
+ * @author niuzhihuan
+ */
+package v2.ch05;
