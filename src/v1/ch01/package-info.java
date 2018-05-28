881eda58528c407c3a242f27d8b787e07ba5a209
niuzhihuan <zhihuan.niu@funi.com>
2018-05-28 10:01:11 +0800

    1

---
+++ src/v1/ch01/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Java程序设计概述
+ * 
+ * Java程序设计平台：Java是一个完整的平台，有一个庞大的库。
+ * Java"白皮书"的关键术语
+ * 	简单性、面向对象、网络技能、健壮性、安全性、体系结构中立、可移植性、解释型、高性能、多线程、动态性。
+ * Java applet与Internet
+ * Java发展简史
+ * 关于Java的常见误解
+ * @author niuzhihuan
+ */
+package v1.ch01;
--- src/v1/ch05/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author zhihuan.niu
- * @date 2017-11-22 9:21:13 AM
- * @Description: 继承
- */
-package v1.ch05;
