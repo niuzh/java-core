7a9d1a43a52a0319c96bbf0faa742bd20558968c
niuzhihuan <zhihuan.niu@funi.com>
2018-06-04 21:54:11 +0800

    继承

---
+++ src/v1/ch05/abstractClasses/Person.java
@@ -0,0 +1,15 @@
+package v1.ch05.abstractClasses;
+
+public abstract class Person {
+	public abstract String getDescription();
+
+	private String name;
+
+	public Person(String n) {
+		name = n;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
