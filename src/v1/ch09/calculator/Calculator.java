c8dec58b05bcbda11340bd2e7a0f0748475af69c
niuzhihuan <zhihuan.niu@funi.com>
2018-06-06 20:51:42 +0800

    事件处理

---
+++ src/v1/ch09/calculator/Calculator.java
@@ -0,0 +1,21 @@
+package v1.ch09.calculator;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @version 1.33 2007-06-12
+ * @author Cay Horstmann
+ */
+public class Calculator {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				CalculatorFrame frame = new CalculatorFrame();
+				frame.setTitle("Calculator");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+}
